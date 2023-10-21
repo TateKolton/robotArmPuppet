e4ef87a3da44923ffc513d27da4b261e3ca0d8f8
Tate Kolton <tatekolton@gmail.com>
2023-10-20 17:25:16 -0700

    initial commit

---
+++ armFirmware.cpp
@@ -0,0 +1,284 @@
+/*
+Created By: Tate Kolton, Parsa Khodabakhshi
+Created On: October 15, 2023
+Description: Teensy 4.1 code for driving a 6 axis robot arm and communicating with a host PC software
+*/
+
+// header file with all constants defined and libraries included
+#include "armFirmware.h"
+
+// setup function to initialize pins and provide initial homing to the arm.
+void setup() {
+
+  Serial.begin(9600);
+
+  for (int i = 0; i < NUM_AXES; i++) {
+    min_steps[i] = -homeCompSteps[i];
+    max_steps[i] = max_steps[i] - homeCompSteps[i];
+  }
+
+  min_steps[0] = -max_steps[0];
+
+  // initializes end effector motor
+  pinMode(EEstepPin, OUTPUT);
+  pinMode(EEdirPin, OUTPUT);
+  endEff.setMinPulseWidth(200);
+  endEff.setMaxSpeed(speedEE);
+  endEff.setAcceleration(accEE);
+  endEff.setCurrentPosition(1000);
+
+  // initializes step pins, direction pins, limit switch pins, and stepper motor objects for accelStepper library
+  for (i = 0; i < NUM_AXES; i++) {
+    pinMode(dirPins[i], OUTPUT);
+    pinMode(stepPins[i], OUTPUT);
+    pinMode(limPins[i], INPUT_PULLUP);
+    steppers[i] = AccelStepper(1, stepPins[i], dirPins[i]);
+    steppers[i].setMinPulseWidth(200);
+    steppers[i].setCurrentPosition(0);
+  }
+}
+
+// main program loop
+void loop() {
+
+  // receives command from serial and executes accoringly
+  recieveCommand();
+
+  // run steppers to target position
+  runSteppers();
+}
+
+void recieveCommand() {
+  String inData = "";
+  char recieved = '\0';
+  char firstChar;
+
+  // if a message is present, copy it to inData
+  if (Serial.available() > 0) {
+
+    firstChar = Serial.read();
+    if(firstChar == 'S') { // start char for message
+      while (recieved != '\n') {
+        recieved = Serial.read();
+        inData += String(recieved);
+      }
+      parseMessage(function, inData);
+    }
+  }
+}
+
+void parseMessage(String inMsg) {
+  String function = inMsg.substring(0, 2);
+
+  // choose which function to run
+  if (function == "MT") {
+    commandArmPose();
+  }
+
+  else if(function == "EE"){
+    endEffectorCommands(inMsg);
+  }
+
+  else if(function == "PS") {
+    executePoseSequence(inMsg);
+  }
+
+  else if (function == "HM") {
+    homeArm();
+  }
+
+  // Send arm angles and gripper force to hardware driver
+  sendArmFeedback();
+}
+
+// Move arm based on inputted angles
+void commandArmPose(String inMsg) {
+  // get new position commands
+
+  int msgIdxJ1 = inMsg.indexOf('A');
+  int msgIdxJ2 = inMsg.indexOf('B');
+  int msgIdxJ3 = inMsg.indexOf('C');
+  int msgIdxJ4 = inMsg.indexOf('D');
+  int msgIdxJ5 = inMsg.indexOf('E');
+  int msgIdxJ6 = inMsg.indexOf('F');
+  int msgIdxJ7 = inMsg.indexOf('G');
+  cmdArmAngle[0] = inMsg.substring(msgIdxJ1 + 1, msgIdxJ2).toInt();
+  cmdArmAngle[1] = inMsg.substring(msgIdxJ2 + 1, msgIdxJ3).toInt();
+  cmdArmAngle[2] = inMsg.substring(msgIdxJ3 + 1, msgIdxJ4).toInt();
+  cmdArmAngle[3] = inMsg.substring(msgIdxJ4 + 1, msgIdxJ5).toInt();
+  cmdArmAngle[4] = inMsg.substring(msgIdxJ5 + 1, msgIdxJ6).toInt();
+  cmdArmAngle[5] = inMsg.substring(msgIdxJ6 + 1, msgIdxJ7).toInt();
+  cmdEEPct = inMsg.substring(msgIdxJ7 + 1).toInt;
+
+  angleToSteps();
+  cmdArm();
+  cmdEndEff();
+}
+
+// This code will execute a sequence of poses specified via incoming serial message inMsg. This function blocks readings from puppet controller until sequence is completed. 
+void executePoseSequence(String inMsg) {
+
+}
+
+// Handles homing of end effector
+void homeEndEffector(String inMsg) {
+
+}
+
+// Send arm angles back to PC
+void sendArmFeedback() {
+
+  char garbage;
+  while (Serial.available() > 0) {
+    garbage = Serial.read();
+  }
+  
+  String outMsg = String("JP") + String("A") + String(steppers[0].currentPosition()) + String("B") + String(steppers[1].currentPosition()) + String("C") + String(steppers[2].currentPosition())
+                  + String("D") + String(steppers[3].currentPosition()) + String("E") + String(steppers[4].currentPosition()) + String("F") + String(steppers[5].currentPosition()) + String("G") + String(endEff.currentPosition()) + String("Z");
+  Serial.print(outMsg);
+}
+
+// execute steppers to target positions
+void runSteppers() {
+
+  for (i = 0; i < NUM_AXES; i++) {
+    steppers[i].run();
+  }
+
+  endEff.run();
+}
+
+// sets Joint Mode speeds after switching out of cartesian mode
+void setJointSpeed() {
+  for (i = 0; i < NUM_AXES; i++) {
+    steppers[i].setMaxSpeed(maxSpeed[i]);
+    steppers[i].setAcceleration(maxAccel[i]);
+  }
+}
+
+// converts angles to steps for motors
+void angleToSteps() {
+  for(int i=0; i<NUM_AXES; i++){
+    cmdArmSteps[i] = cmdArmAngle[i]*axisDir[i]*ppr[i]*red[i]/360.0;
+
+    if(cmdArmSteps[i] > max_steps[i]) {
+      cmdArmSteps[i] = max_steps[i];
+    }
+
+    else if(cmdArmSteps[i] < min_steps[i]) {
+      cmdArmSteps[i] = min_steps[i];
+    }
+  }
+
+  cmdEESteps = 
+}
+
+// sets target positions (in steps) for motors
+void cmdArm() {
+  for(int i=0; i<NUM_AXES; i++) {
+    steppers[i].moveTo(cmdArmSteps[i]);
+  }
+}
+
+//****// ARM CALIBRATION FUNCTIONS//****//
+
+void homeArm() {  // main function for full arm homing
+
+  initializeHomingMotion();
+  homeAxes();
+  initializeMotion();
+}
+
+// Runs through and checks if each axis has reached its limit switch, then runs it to specified home position
+void homeAxes() {
+  bool stopFlags[6] = { false, false, false, false, false, false };
+  bool finishFlags[6] = { false, false, false, false, false, false };
+  bool completeFlag = false;
+  int count = 0;
+
+  while (!completeFlag) {
+
+    for (i = 0; i < NUM_AXES; i++) {
+
+      if (!finishFlags[i]) {
+
+        if (!digitalRead(limPins[i]) && !stopFlags[i]) {
+          steppers[i].run();
+        }
+
+        else if (!stopFlags[i]) {
+          steppers[i].setCurrentPosition(-homeComp[i]);
+          steppers[i].stop();
+          steppers[i].setMaxSpeed(maxSpeed[i] / 2);
+          steppers[i].setAcceleration(homeAccel[i]);
+          steppers[i].moveTo(homeCompSteps[i]);
+          stopFlags[i] = true;
+        }
+
+        else if (steppers[i].distanceToGo() != 0) {
+          steppers[i].run();
+        }
+
+        else {
+          finishFlags[i] = true;
+          count++;
+        }
+      }
+    }
+    if (count == NUM_AXES) {
+      completeFlag = true;
+    }
+  }
+}
+
+void initializeHomingMotion() {  // sets homing speed and acceleration and sets target homing position
+
+  for (i = 0; i < NUM_AXES; i++) {
+
+    steppers[i].setMaxSpeed(homeSpeed[i]);
+    steppers[i].setAcceleration(homeAccel[i]);
+
+    if (i == 0)  // Special case for axis 1
+    {
+      if ((axisDir[0] * steppers[0].currentPosition() < -homeCompSteps[0])) {
+        steppers[0].move(-homePos[i]);
+      } else {
+        steppers[0].move(homePos[i]);
+      }
+    }
+
+    else {
+      steppers[i].move(homePos[i]);
+    }
+  }
+}
+
+void initializeMotion() {  // sets main program speeds for each axis and zeros position
+
+  setJointSpeed();
+
+  for (int i = 0; i < NUM_AXES; i++) {
+    steppers[i].setCurrentPosition(0);
+  }
+}
+
+void waitForHome() {
+  String inData;
+  char recieved = '\0';
+
+  while (!initFlag) {
+    inData = "";
+    if (Serial.available() > 0) {
+      do {
+        recieved = Serial.read();
+        inData += String(recieved);
+      } while (recieved != '\n');
+    }
+
+    if (recieved == '\n') {
+      if (inData.substring(0, 2) == "HM")
+        homeArm();
+      initFlag = true;
+    }
+  }
+}
---
+++ armFirmware.h
@@ -0,0 +1,140 @@
+/*
+Created By: Tate Kolton, Parsa Khodabakhshi
+Created On: October 15, 2023
+Description: Header file for driving a 6 axis robot arm and communicating with a host PC software
+*/
+
+#include <AccelStepper.h>
+#include <Encoder.h>
+
+// general parameters
+#define NUM_AXES 6
+#define ON 0
+#define OFF 1
+#define SW_ON 0
+#define SW_OFF 1
+#define FWD 1
+#define REV 0
+
+// Motor pins
+int stepPins[6] =   {6, 8, 2, 10, 12, 25}; 
+int dirPins[6] =    {5, 7, 1, 9, 11, 24}; 
+
+// Encoder pins
+int encPinA[6] = {17, 38, 40, 36, 13, 15};
+int encPinB[6] = {16, 37, 39, 35, 41, 14};
+
+// limit switch pins
+int limPins[6] = {18, 19, 20, 21, 23, 22};
+
+// pulses per revolution for motors
+long ppr[6] = {400, 400, 400, 400, 400, 400};
+
+// Gear Reductions
+float red[6] = {50.0, 160.0, 92.3077, 43.936, 57.0, 14.0};
+
+// End effector variables
+const int closePos = 0;
+const int openPos = 500; 
+const int EEstepPin = 4;
+const int EEdirPin = 3;
+const int speedEE = 500;
+const int accEE = 500;
+const int MOTOR_DIR_EE = 1;
+const int forcePin = 12;
+
+
+// Position
+double currPos[NUM_AXES], cmdArmAngle[NUM_AXES];
+double cmdEEPct;
+int cmdArmSteps[NUM_AXES];
+int cmdEESteps;
+int endEffMax = 1000;
+
+// Motor speeds and accelerations
+const int maxSpeed[6] = {1000, 1800, 2000, 2000, 3000, 2000};
+const int maxAccel[6] = {2000, 3000, 3000, 3300, 4000, 3000};
+const int homeSpeed[6] = {1500, 2000, 2000, 2500, 1500, 1000}; 
+const int homeAccel[6] = {1500, 2000, 2000, 2500, 1500, 1000}; 
+
+// Stepper motor objects for AccelStepper library
+AccelStepper steppers[6];
+AccelStepper endEff(1, EEstepPin, EEdirPin);
+
+// Encoder objects for Encoder library
+Encoder enc1(encPinA[0], encPinB[0]);
+Encoder enc2(encPinA[1], encPinB[1]);
+Encoder enc3(encPinA[2], encPinB[2]);
+Encoder enc4(encPinA[3], encPinB[3]);
+Encoder enc5(encPinA[4], encPinB[4]);
+Encoder enc6(encPinA[5], encPinB[5]);
+
+// General Global Variable declarations
+const int axisDir[6] = {1, -1, -1, 1, -1, -1};  
+int i;
+bool initFlag = false;
+bool jointFlag = false; 
+bool IKFlag = false;
+bool resetEE = false;
+
+// Variables for homing / arm calibration
+long homePosConst = -99000;
+long homePos[] = {axisDir[0]*homePosConst, axisDir[1]*homePosConst, axisDir[2]*homePosConst, axisDir[3]*homePosConst, axisDir[4]*homePosConst, axisDir[5]*homePosConst};
+long homeCompAngles[] = {50, 0, 0, 100, 30, 90};
+long homeCompConst[] = {500, 1000, 1000, 500, 500, 500};
+long homeComp[] = {axisDir[0]*homeCompConst[0], axisDir[1]*homeCompConst[1], axisDir[2]*homeCompConst[2], axisDir[3]*homeCompConst[3], axisDir[4]*homeCompConst[4], axisDir[5]*homeCompConst[5]};
+long homeCompSteps[] = {axisDir[0]*homeCompAngles[0]*red[0]*ppr[0]/360.0, axisDir[1]*homeCompAngles[1]*red[1]*ppr[1]/360.0, axisDir[2]*homeCompAngles[2]*red[2]*ppr[2]/360.0, axisDir[3]*homeCompAngles[3]*red[3]*ppr[3]/360.0, axisDir[4]*homeCompAngles[4]*red[4]*ppr[4]/360.0, axisDir[5]*homeCompAngles[5]*red[5]*ppr[5]/360.0};
+
+// Range of motion (degrees) for each axis
+int maxAngles[6] = {200, 100, 180, 180, 120, 320};
+long max_steps[] = {axisDir[0]*red[0]*maxAngles[0]/360.0*ppr[0], axisDir[1]*red[1]*maxAngles[1]/360.0*ppr[1], axisDir[2]*red[2]*maxAngles[2]/360.0*ppr[2], axisDir[3]*red[3]*maxAngles[3]/360.0*ppr[3], axisDir[4]*red[4]*maxAngles[4]/360.0*ppr[4], axisDir[5]*red[5]*maxAngles[5]/360.0*ppr[5]};
+long min_steps[NUM_AXES]; 
+char value;
+
+
+//****// FUNCTION PROTOTYPES //****//
+
+// Waits for a message to be published to the serial port.
+void recieveCommand();
+
+// Parses the message passed to it and determines which subfunction to call for control of arm.
+void parseMessage(String inMsg);
+
+// Sets the max speed and acceleration for each joint and motor to its Joint Mode speed.
+void setJointSpeed();
+
+// Prints the current encoder position for each axis to serial.
+void sendArmFeedback();
+
+// Sets target position for all motors
+int cmdArm();
+
+// Main function for homing the entire arm.
+void homeArm();
+
+// Runs through and checks if each axis has reached its limit switch, then runs it a couple steps away from switch for 0 position.
+void homeAxes();
+
+// sets homing speed and acceleration for axes 1-6 and sets target homing position.
+void initializeHomingMotion();
+
+// sets main program speeds for each axis.
+void initializeMotion();
+
+// Runs all stepper motors to target position (if no target defined, motors will not move).
+void runSteppers();
+
+// Waits for the user to press the home button before continuing with other functions. Reads serial port and homes the arm if "HM___" is read.
+void waitForHome();
+
+// Move arm to controller specified pose
+void commandArmPose();
+
+// Convert axes angles to motor steps
+void angleToSteps();
+
+// This code will execute a sequence of poses specified via incoming serial message inMsg. This function blocks readings from puppet controller until sequence is completed. 
+void executePoseSequence(String inMsg);
+
+// Homes end effector
+void homeEndEffector(String inMsg);
